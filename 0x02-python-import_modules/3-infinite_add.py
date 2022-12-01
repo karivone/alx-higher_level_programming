8084ecf6639aa3fd7865c60696e4a0ea1fd91d2d
karivone <karimiyvonne19@gmail.com>
2022-12-01 20:57:03 +0000

    adding infinitely

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
 
 if __name__ == '__main__':
+    import sys
 
-    from sys import argv
-
-    argc = len(argv)
-    if argc == 1:
-        add = 0
-    add = 0
-    index = 1
-    while index < argc:
-        add += int(argv[index])
-        index += 1
-    print(add)
+    index = len(sys.argv) - 1
+    total = 0    
+    for b in range(index):
+        total += int(sys.argv[b+1])
+    print("{}".format(total))
