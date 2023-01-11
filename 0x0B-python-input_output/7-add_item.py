078f42f7d34cfa933ffd42cca4a9f4797ea274a1
karivone <karimiyvonne19@gmail.com>
2023-01-11 12:34:14 +0300

    python imputs and outputs

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,17 +1,27 @@
 #!/usr/bin/python3
-"""A script that adds all arguments to a
-Python list, and then save them to a file"""
+"""
+Module 7-add_item
+Contains function that adds and saves to Python obj to JSON file; loads objects
+# run with ./7-add_item.py
+#
+# cat add_item.json ; echo ""
+# expect output: []
+#
+# ./7-add_item.py some random args
+# cat add_item.json ; echo ""
+# expect output: ["some", "random", "args"]
+"""
+
 
 from sys import argv
-save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
-try:
-    json_list = load_from_json_file(filename)
-except(Exception):
-    json_list = []
 
-for elm in argv[1:]:
-    json_list.append(elm)
-save_to_json_file(json_list, filename)
+try:
+    existing_content = load_from_json_file(filename)
+except FileNotFoundError:
+    existing_content = []
+
+save_to_json_file(existing_content + argv[1:], filename)
