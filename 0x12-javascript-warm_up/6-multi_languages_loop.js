33a7d2c2d2ebcfb853a2373ff709dd5a54300c76
karivone <karimiyvonne19@gmail.com>
2023-03-14 01:04:21 +0300

    javascript warm up

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,5 +2,5 @@
 
 const lines = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 for (const l of lines) {
-    console.log(l);
+  console.log(l);
 }
--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,9 +2,9 @@
 
 const num = parseInt(process.argv[2], 10);
 if (isNaN(num)) {
-    console.log('Missing number of occurences');
+  console.log('Missing number of occurences');
 } else {
-    for (let i = num; i > 0; i -= 1) {
-        console.log('C is fun');
-    }
+  for (let i = num; i > 0; i -= 1) {
+    console.log('C is fun');
+  }
 }
