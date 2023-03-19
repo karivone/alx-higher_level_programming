d865e2c89199fb6b93bff96ccff1d0be8f81dd1f
kayewi <55494837+karivone@users.noreply.github.com>
2023-03-19 22:10:49 +0300

    Update 4-cities_by_states.sql

--- 0x0F-python-object_relational_mapping/4-cities_by_states.sql
+++ 0x0F-python-object_relational_mapping/4-cities_by_states.sql
@@ -1,116 +1,41 @@
 -- Create states table in hbtn_0e_4_usa with some data
-CREATE DATABASE IF NOT EXISTS HBTN_0E_4_USA;
-
-USE HBTN_0E_4_USA;
-
-CREATE TABLE IF NOT EXISTS STATES (
-    ID INT NOT NULL AUTO_INCREMENT,
-    NAME VARCHAR(256) NOT NULL,
-    PRIMARY KEY (ID)
+CREATE DATABASE IF NOT EXISTS hbtn_0e_4_usa;
+USE hbtn_0e_4_usa;
+CREATE TABLE IF NOT EXISTS states (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id)
 );
-
-INSERT INTO STATES (
-    NAME
-) VALUES (
-    "California"
-),
-(
-    "Arizona"
-),
-(
-    "Texas"
-),
-(
-    "New York"
-),
-(
-    "Nevada"
+INSERT INTO states (name)
+VALUES ("California"),
+    ("Arizona"),
+    ("Texas"),
+    ("New York"),
+    ("Nevada");
+CREATE TABLE IF NOT EXISTS cities (
+    id INT NOT NULL AUTO_INCREMENT,
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY(state_id) REFERENCES states(id)
 );
-
-CREATE TABLE IF NOT EXISTS CITIES (
-    ID INT NOT NULL AUTO_INCREMENT,
-    STATE_ID INT NOT NULL,
-    NAME VARCHAR(256) NOT NULL,
-    PRIMARY KEY (ID),
-    FOREIGN KEY(STATE_ID) REFERENCES STATES(ID)
-);
-
-INSERT INTO CITIES (
-    STATE_ID,
-    NAME
-) VALUES (
-    1,
-    "San Francisco"
-),
-(
-    1,
-    "San Jose"
-),
-(
-    1,
-    "Los Angeles"
-),
-(
-    1,
-    "Fremont"
-),
-(
-    1,
-    "Livermore"
-);
-
-INSERT INTO CITIES (
-    STATE_ID,
-    NAME
-) VALUES (
-    2,
-    "Page"
-),
-(
-    2,
-    "Phoenix"
-);
-
-INSERT INTO CITIES (
-    STATE_ID,
-    NAME
-) VALUES (
-    3,
-    "Dallas"
-),
-(
-    3,
-    "Houston"
-),
-(
-    3,
-    "Austin"
-);
-
-INSERT INTO CITIES (
-    STATE_ID,
-    NAME
-) VALUES (
-    4,
-    "New York"
-);
-
-INSERT INTO CITIES (
-    STATE_ID,
-    NAME
-) VALUES (
-    5,
-    "Las Vegas"
-),
-(
-    5,
-    "Reno"
-),
-(
-    5,
-    "Henderson"
-),
-(
-    5,
-    "Carson City"
-);
+INSERT INTO cities (state_id, name)
+VALUES (1, "San Francisco"),
+    (1, "San Jose"),
+    (1, "Los Angeles"),
+    (1, "Fremont"),
+    (1, "Livermore");
+INSERT INTO cities (state_id, name)
+VALUES (2, "Page"),
+    (2, "Phoenix");
+INSERT INTO cities (state_id, name)
+VALUES (3, "Dallas"),
+    (3, "Houston"),
+    (3, "Austin");
+INSERT INTO cities (state_id, name)
+VALUES (4, "New York");
+INSERT INTO cities (state_id, name)
+VALUES (5, "Las Vegas"),
+    (5, "Reno"),
+    (5, "Henderson"),
+    (5, "Carson City");
